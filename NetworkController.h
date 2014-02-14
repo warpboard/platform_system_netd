a7526d3a6f72077bc0df3d37be63758b53ff7652
Szymon Jakubczak <szym@google.com>
2014-02-14 17:09:43 -0500

    netd: Replace iface with opaque netid in resolver.
    Also ensure that UID mapping (for VPN) cannot be overridden by
    android_getaddrinfofornet or per-PID preference.
    
    Change-Id: I9ccfda2902cc0943e87c9bc346ad9a2578accdab

---
+++ NetworkController.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _NETD_NETWORKCONTROLLER_H
+#define _NETD_NETWORKCONTROLLER_H
+
+#include <list>
+#include <map>
+#include <string>
+
+#include <stddef.h>
+#include <stdint.h>
+#include <utils/RWLock.h>
+
+/*
+ * Keeps track of default, per-pid, and per-uid-range network selection, as
+ * well as the mark associated with each network. Networks are identified
+ * by netid. In all set* commands netid == 0 means "unspecified" and is
+ * equivalent to clearing the mapping.
+ */
+class NetworkController {
+public:
+    NetworkController();
+
+    void setDefaultNetwork(unsigned netid);
+    void setNetworkForPid(int pid, unsigned netid);
+    // Returns false if a partially overlapping range exists.
+    bool setNetworkForUidRange(int uid_start, int uid_end, unsigned netid,
+                               bool forward_dns);
+    void clearNetworkPreference();
+
+    unsigned getDefaultNetwork() const;
+    // Order of preference: UID-specific, netid, PID-specific, default.
+    unsigned getNetwork(int uid, unsigned netid, int pid, bool for_dns) const;
+
+    unsigned getNetworkId(const char* interface);
+
+private:
+    struct UidEntry {
+        int uid_start;
+        int uid_end;
+        unsigned netid;
+        bool forward_dns;
+        UidEntry(int uid_start, int uid_end, unsigned netid, bool forward_dns);
+    };
+
+    mutable android::RWLock mRWLock;
+    std::list<UidEntry> mUidMap;
+    std::map<int, unsigned> mPidMap;
+    unsigned mDefaultNetId;
+
+    std::map<std::string, unsigned> mIfaceNetidMap;
+    unsigned mNextFreeNetid;
+};
+
+#endif
